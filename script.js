40959c6d1fa08751fe5e762c8d2061beeb593c0a
tula2110 <phongares2110@gmail.com>
2024-11-22 12:16:45 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+function calculate(operator) {
+  const num1 = document.getElementById('number1').value;
+  const num2 = document.getElementById('number2').value;
+
+  if (num1 === '' || num2 === '') {
+    alert('Two input can not empty!');
+    return;
+  }
+
+  const n1 = parseFloat(num1);
+  const n2 = parseFloat(num2);
+
+  if (operator === '/' && n2 === 0) {
+    alert('Division by 0 is not allowed!');
+    return;
+  }
+
+  let result;
+  switch (operator) {
+    case '+':
+      result = n1 + n2;
+      break;
+    case '-':
+      result = n1 - n2;
+      break;
+    case '*':
+      result = n1 * n2;
+      break;
+    case '/':
+      result = n1 / n2;
+      break;
+    default:
+      result = 'Invalid operator';
+  }
+
+  document.getElementById('result').textContent = `Result: ${result}`;
+}
